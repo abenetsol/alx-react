dbe6c1a6ed3ba41ee1ffc88b3014905b8d0d4de0
abenetsol <solabenet@gmail.com>
2023-03-16 05:41:57 -0700

    task 7

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,8 +1,7 @@
-/*
-Create a function named areMapsEqual
-It accepts two arguments map1 and map2. Both are Immutable.js Maps
-It should return true if the Maps are equal
-*/
+import { is } from 'immutable';
 export default function areMapsEqual(map1, map2) {
-  return map1.equals(map2);
+  if (is(map1, map2)) {
+    return true;
+  }
+  return false;
 }
